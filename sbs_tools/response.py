33e8f499c7d3399ce1884b95148c7e04a22f9d13
Yannis Angelis <ioannis.angelis@cern.ch>
2025-02-25 16:21:56 +0100

    Extract some common functionality
    Knob and attribute response now use the same function to create and fill
    the arrays

--- sbs_tools/response.py
+++ sbs_tools/response.py
@@ -1,4 +1,4 @@
-import xtrack as xt
+from xtrack import TwissTable
 import numpy as np
 from typing import Sequence
 
@@ -6,55 +6,109 @@ from typing import Sequence
 from .segment import Segment
 
 
+def _create_res_array(n1: int, n2: int):
+    betax = np.zeros(shape=(n1, n2), dtype=np.float64)
+    betay = np.zeros(shape=(n1, n2), dtype=np.float64)
+
+    betabeatx = np.zeros(shape=(n1, n2), dtype=np.float64)
+    betabeaty = np.zeros(shape=(n1, n2), dtype=np.float64)
+
+    dxs = np.zeros(shape=(n1, n2), dtype=np.float64)
+    dys = np.zeros(shape=(n1, n2), dtype=np.float64)
+
+    ndxs = np.zeros(shape=(n1, n2), dtype=np.float64)
+    ndys = np.zeros(shape=(n1, n2), dtype=np.float64)
+
+    dmuxs = np.zeros(shape=(n1, n2), dtype=np.float64)
+    dmuys = np.zeros(shape=(n1, n2), dtype=np.float64)
+
+    return betax, betay, betabeatx, betabeaty, dxs, dys, ndxs, ndys, dmuxs, dmuys
+
+
+def _fill_res_array(
+    i: int,
+    twk: TwissTable,
+    twm: TwissTable,
+    deltak: float,
+    betax: np.ndarray,
+    betay: np.ndarray,
+    betabeatx: np.ndarray,
+    betabeaty: np.ndarray,
+    dxs: np.ndarray,
+    dys: np.ndarray,
+    ndxs: np.ndarray,
+    ndys: np.ndarray,
+    dmuxs: np.ndarray,
+    dmuys: np.ndarray,
+):
+    betax[:, i] = twk.betx
+    betay[:, i] = twk.bety
+
+    betabeatx[:, i] = ((betax[:, i] - twm.betx) / twm.betx) / deltak
+    betabeaty[:, i] = ((betay[:, i] / twm.bety) / twm.bety) / deltak
+
+    dxs[:, i] = (twk.dx - twm.dx) / deltak
+    dys[:, i] = (twk.dy - twm.dy) / deltak
+
+    ndxs[:, i] = (twk.dx / np.sqrt(twk.betx) - twm.dx / np.sqrt(twm.betx)) / deltak
+    ndys[:, i] = (twk.dy / np.sqrt(twk.bety) - twm.dy / np.sqrt(twm.bety)) / deltak
+
+    dmuxs[:, i] = (twk.mux - twm.mux) / deltak
+    dmuys[:, i] = (twk.muy - twm.muy) / deltak
+    return
+
+
 def create_response(
     segment: Segment,
     magnet_names: Sequence[str],
     bpms: np.ndarray,
     attr: str = "k1",
-    delta_k: float = 2e-5,
-    mode="front",
+    deltak: float = 2e-5,
+    mode: str = "front",
 ) -> dict[str, np.ndarray]:
 
     nknobs = len(magnet_names)
 
-    betax = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-    betay = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-
-    betabeatx = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-    betabeaty = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-
-    dxs = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-    dys = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-
-    dmuxs = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-    dmuys = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
+    betax, betay, betabeatx, betabeaty, dxs, dys, ndxs, ndys, dmuxs, dmuys = (
+        _create_res_array(bpms.shape[1], nknobs)
+    )
 
     tw_sbs = segment.twiss(mode=mode)
 
     for i, mname in enumerate(magnet_names):
         original_val = getattr(segment.line.element_dict[mname], attr)
-        setattr(segment.line.element_dict[mname], attr, original_val + delta_k)
+        setattr(segment.line.element_dict[mname], attr, original_val + deltak)
 
         tw_dk = segment.twiss(mode=mode)
 
         setattr(segment.line.element_dict[mname], attr, original_val)
 
-        betax[:, i] = tw_dk.rows[bpms[0, :]].betx
-        betay[:, i] = tw_dk.rows[bpms[0, :]].bety
-
-        dxs[:, i] = (tw_dk.rows[bpms[0, :]].dx - tw_sbs.rows[bpms[0, :]].dx) / delta_k
-        dys[:, i] = (tw_dk.rows[bpms[0, :]].dy - tw_sbs.rows[bpms[0, :]].dy) / delta_k
-
-        dmuxs[:, i] = (
-            tw_dk.rows[bpms[0, :]].mux - tw_sbs.rows[bpms[0, :]].mux
-        ) / delta_k
-        dmuys[:, i] = (
-            tw_dk.rows[bpms[0, :]].muy - tw_sbs.rows[bpms[0, :]].muy
-        ) / delta_k
+        _fill_res_array(
+            i,
+            tw_dk.rows[bpms[0, :]],
+            tw_sbs.rows[bpms[0, :]],
+            deltak,
+            betax,
+            betay,
+            betabeatx,
+            betabeaty,
+            dxs,
+            dys,
+            ndxs,
+            ndys,
+            dmuxs,
+            dmuys,
+        )
 
     return {
+        "BETX": betax,
+        "BETY": betay,
+        "DBETX": betabeatx,
+        "DBETY": betabeaty,
         "DX": dxs,
         "DY": dys,
+        "NDX": ndxs,
+        "NDY": ndys,
         "DMUX": dmuxs,
         "DMUY": dmuys,
     }
@@ -64,47 +118,53 @@ def create_knob_response(
     segment: Segment,
     knob_names: Sequence[str],
     bpms: np.ndarray,
-    delta: float = 2e-5,
-    mode="front",
+    deltak: float = 2e-5,
+    mode: str = "front",
 ) -> dict[str, np.ndarray]:
 
+    if mode not in ["front", "back"]:
+        print("Available modes are 'front' or 'back' ")
     nknobs = len(knob_names)
 
-    betax = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-    betay = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-
-    betabeatx = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-    betabeaty = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-
-    dxs = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-    dys = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-
-    dmuxs = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
-    dmuys = np.zeros(shape=(bpms.shape[1], nknobs), dtype=np.float64)
+    betax, betay, betabeatx, betabeaty, dxs, dys, ndxs, ndys, dmuxs, dmuys = (
+        _create_res_array(bpms.shape[1], nknobs)
+    )
 
     tw_sbs = segment.twiss(mode=mode)
 
     for i, kname in enumerate(knob_names):
         # NOTE: str(kname) instead of kname to avoid np.str_
         original_val = segment.line.varval[str(kname)]
-        segment.line.vars[str(kname)] += delta
+        segment.line.vars[str(kname)] += deltak
 
         tw_dk = segment.twiss(mode=mode)
 
-        segment.line.vars[str(kname)] -= delta
+        segment.line.vars[str(kname)] = original_val
 
-        betax[:, i] = tw_dk.rows[bpms[0, :]].betx
-        betay[:, i] = tw_dk.rows[bpms[0, :]].bety
-
-        dxs[:, i] = (tw_dk.rows[bpms[0, :]].dx - tw_sbs.rows[bpms[0, :]].dx) / delta
-        dys[:, i] = (tw_dk.rows[bpms[0, :]].dy - tw_sbs.rows[bpms[0, :]].dy) / delta
-
-        dmuxs[:, i] = (tw_dk.rows[bpms[0, :]].mux - tw_sbs.rows[bpms[0, :]].mux) / delta
-        dmuys[:, i] = (tw_dk.rows[bpms[0, :]].muy - tw_sbs.rows[bpms[0, :]].muy) / delta
+        _fill_res_array(
+            i,
+            tw_dk.rows[bpms[0, :]],
+            tw_sbs.rows[bpms[0, :]],
+            deltak,
+            betax,
+            betay,
+            betabeatx,
+            betabeaty,
+            dxs,
+            dys,
+            ndxs,
+            ndys,
+            dmuxs,
+            dmuys,
+        )
 
     return {
+        "DBETX": betabeatx,
+        "DBETY": betabeaty,
         "DX": dxs,
         "DY": dys,
+        "NDX": ndxs,
+        "NDY": ndys,
         "DMUX": dmuxs,
         "DMUY": dmuys,
     }
@@ -114,9 +174,9 @@ def tw_strengths_deltak(
     sbs: Segment,
     magnet_names: Sequence[str],
     dks: Sequence[float],
-    mode="front",
+    mode: str = "front",
     attr: str = "k1",
-) -> xt.TwissTable:
+) -> TwissTable:
 
     original_values = []
     for i, imq in enumerate(magnet_names):
